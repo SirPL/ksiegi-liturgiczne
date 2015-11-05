a487e785bc9922121877307f60847c52df85a24e
SirPL <ciemakat@gmail.com>
2015-11-05 23:24:09 +0100

    Loader change
    Changing way of loading side menu

--- include/loader.js
+++ include/loader.js
@@ -4,13 +4,10 @@ $.routes({'/': function(){$('.wrap-main').load('sites/home.html',function(respon
                 $('.wrap-main').load("404.html");
             }
         });
-        var response = $.ajax({
-            url: 'aside_menu.html',
-            success: function(result){
-                $('.wrap-aside' ).html(result);
-                colorize();
-            },
-            async: false
+        $(".wrap-aside").load('sites/' + params.folder + '/_aside_menu.html', function(response, status, xhr) {
+            if(status == "error") {
+                $(".wrap-aside").html("");
+            }
         });
         colorize();
     },
@@ -22,14 +19,12 @@ $.routes({'/': function(){$('.wrap-main').load('sites/home.html',function(respon
                 $('.wrap-main').load("404.html");
             }
         });
-        $.ajax({
-            url: 'aside_menu.html',
-            success: function(result){
-                $('.wrap-aside' ).html(result);
-                colorize();
-            },
-            async: false
+        $(".wrap-aside").load('sites/' + params.folder + '/_aside_menu.html', function(response, status, xhr) {
+            if(status == "error") {
+                $(".wrap-aside").html("");
+            }
         });
+        colorize();
     },
     '/:folder/:name': function(params){
         $('.wrap-main').load('sites/' + params.folder +
@@ -40,46 +35,27 @@ $.routes({'/': function(){$('.wrap-main').load('sites/home.html',function(respon
                 $('.wrap-main').load("404.html");
             }
         });
-
-        $.ajax({
-            url: 'sites/' + params.folder +
-            '/_aside_menu.html',
-            success: function(result){
-                $('.wrap-aside' ).html(result);
-                colorize();
-            },
-            error: function(xhr, status, error) {
-                $.ajax({
-                    url: 'aside_menu.html',
-                    success: function(result){
-                        $('.wrap-aside' ).html(result);
-                        colorize();
-                    },
-                    async: false
-                });
-            },
-            async: false
+        $(".wrap-aside").load('sites/' + params.folder + '/_aside_menu.html', function(response, status, xhr) {
+            if(status == "error") {
+                $(".wrap-aside").html("");
+            }
         });
+        colorize();
     },
     '/:folder/:name/:anchor': function(params){
-        /*var regex = /.*#\/(\w*)\/(\w*)\/(\w*)/
-        var url = regex.exec(document.URL);
-        if(!(url[1] == params.folder && url[2] == params.name && url[3] == params.anchor)) {
-	$('.wrap-main').load('sites/' + params.folder +
+        $('.wrap-main').load('sites/' + params.folder +
             '/' + params.name + '.html', function(response,
                                                   status,
                                                   xhr) {
             if(status == "error") {
                 $('.wrap-main').load("404.html");
-                return;
+            } else {
+                $(window).scrollTop($('#' + params.anchor).offset().top);
             }
         });
-        }*/
-        $(window).scrollTop($('#' + /.*#\/(\w*)\/(\w*)\/(\w*)/.exec(document.URL)[3]).offset().top);
     }
 });
 
 function colorize() {
-    $('.wrap-aside > ul a').removeClass("current-page");
     $('.wrap-aside > ul a[href=' + window.location.hash + ']').addClass("current-page");
 }
